0173a183def4ed887facb25831aae86418042baf
Atanu Roy <atanuroy911@gmail.com>
2026-05-22 23:33:14 +0600

    student 2 image update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#0b5fff" />
+      </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
   )

224abd5b031140841919508725db3066df4dfd8a
Atanu Roy <atanuroy911@gmail.com>
2026-07-07 07:51:59 +0600

    no semantic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta name="theme-color" content="#0b5fff" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="9f9784df-8594-4ed0-b42f-b16ee3581cfc"></script>
       </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
